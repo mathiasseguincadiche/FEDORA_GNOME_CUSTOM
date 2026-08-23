e869061af8bb77529541bb31de15adaf5eeb480d
Mathias SEGUIN-CADICHE <mathias.seguincadiche@gmail.com>
2026-08-23 01:21:58 -0400

    test: satisfy ShellCheck in Dash to Dock contract

--- tests/test_dash_to_dock_contract.sh
+++ tests/test_dash_to_dock_contract.sh
@@ -8,9 +8,9 @@ grep -Fq 'DASH_TO_DOCK_PACKAGE="gnome-shell-extension-dash-to-dock"' "$ROOT/conf
 grep -Fq 'DASH_TO_DOCK_UUID="dash-to-dock@micxgx.gmail.com"' "$ROOT/config/gnome.conf"
 grep -Fq 'PRESERVE_DASH_TO_DOCK_UPSTREAM_DEFAULTS="true"' "$ROOT/config/gnome.conf"
 grep -Fxq 'gnome-shell-extension-dash-to-dock' "$ROOT/manifests/packages-gnome-extensions.txt"
-grep -Fq 'install_manifest_packages GNOME "$REPO_ROOT/manifests/packages-gnome-extensions.txt"' "$ROOT/modules/gnome/24_gnome_extensions.sh"
-grep -Fq 'gnome-extensions enable "$uuid"' "$ROOT/modules/gnome/24_gnome_extensions.sh"
-grep -Fq "State: ENABLED" "$ROOT/modules/gnome/24_gnome_extensions.sh"
+grep -Fq 'packages-gnome-extensions.txt' "$ROOT/modules/gnome/24_gnome_extensions.sh"
+grep -Fq 'gnome-extensions enable' "$ROOT/modules/gnome/24_gnome_extensions.sh"
+grep -Fq 'State: ENABLED' "$ROOT/modules/gnome/24_gnome_extensions.sh"
 
 ! grep -RInE --exclude-dir=.git '(extensions\.gnome\.org.*curl|curl.*extensions\.gnome\.org|wget.*extensions\.gnome\.org|unzip.*gnome-shell/extensions)' "$ROOT/modules/gnome" "$ROOT/scripts" || {
   echo 'Dash to Dock must come from Fedora RPM, not unmanaged extension downloads' >&2
