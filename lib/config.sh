3217f67126e6ffa5179f7a84ad65fdce541eedf7
Mathias SEGUIN-CADICHE <mathias.seguincadiche@gmail.com>
2026-08-30 22:11:17 -0400

    Scope REPO_ROOT ShellCheck exception in config library

--- lib/config.sh
+++ lib/config.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# REPO_ROOT is intentionally injected by the repository entrypoints before this library is sourced.
+# shellcheck disable=SC2153
 
 config_load() {
   local file local_override="$REPO_ROOT/config/local.conf"
